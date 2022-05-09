6574a54152f708d6d2169516fa7c08c6ece4c43e
habib <hassan.habib@epfl.ch>
2022-05-09 11:05:34 +0200

    Wait until docker containers have finished to run result script

--- clone.sh
+++ clone.sh
@@ -5,10 +5,24 @@ pack="./ListPack_c++"
 while IFS= read -r pkg
 do
   mkdir -p ./repo/$pkg
+  #check if package have a good name  [a-zA-Z0-9][a-zA-Z0-9_.-]*
+  if [[ $pkg =~ ^[a-zA-Z0-9][a-zA-Z0-9_.-]*$ ]]
+  then
+    name="retro:$pkg"
+    else
+    #get rid of incorrect characters
+    name=$(echo retro:$pkg | sed -e 's/[^a-zA-Z0-9_.-]//g')
+  fi
+  echo "$name"
   cd ./repo/$pkg
   docker run --rm -d  \
+    --cpus=4.0 --cpuset-cpus=0,1,2,3 -e cpuload=100 \
     -v "$(pwd)":/home/retro \
-    --name "$pkg" \
+    --name "$name" \
     project:debian "$pkg"
   cd ../..
 done < "$pack"
+
+
+docker wait $(docker ps -a -f "name=retro:" -q)
+./result_count.sh
